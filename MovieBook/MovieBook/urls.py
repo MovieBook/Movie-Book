6caf0bba8ebddf8bd85c3e09ff94d5c1c81dae05
TsHristov <tsvetanhristov@yahoo.com>
2015-05-24 00:14:16 +0300

    fix some urls

--- MovieBook/MovieBook/urls.py
+++ MovieBook/MovieBook/urls.py
@@ -1,14 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from website import urls as website_urls
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$',  'website.views.login'),
-    url(r'^accounts/auth/$',  'website.views.auth_view'),
-    url(r'^accounts/logout/$', 'website.views.logout'),
-    url(r'^accounts/loggedin/$', 'website.views.loggedin'),
-    url(r'^accounts/invalid/$', 'website.views.invalid_login'),
-    url(r'^accounts/register/$', 'website.views.register_user'),
-    url(r'^accounts/register_success/$', 'website.views.register_success'),
-    url(r'^about/$', 'website.views.about'),
+    url(r'', include(website_urls))
 ]
--- MovieBook/website/urls.py
+++ MovieBook/website/urls.py
@@ -1,3 +1,14 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
+from . import views
 
-#
+urlpatterns = [
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/auth/$', views.auth_view, name='authenticate'),
+    url(r'^accounts/logout/$', views.logout, name='logout'),
+    url(r'^accounts/loggedin/$', views.loggedin, name='loggedin'),
+    url(r'^accounts/invalid/$', views.invalid_login, name='invalid_login'),
+    url(r'^accounts/register/$', views.register_user, name='register'),
+    url(r'^accounts/register_success/$',
+        views.register_success, name='register_success'),
+    url(r'^about/$', views.about, name='about'),
+]
